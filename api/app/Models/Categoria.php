b40085071af34407d734ab9816c8025f206f2858
Pedro José NPS <pedrojosepaiva@outlook.com>
2023-12-10 11:49:18 -0300

    Correcoes nos modulos, incluindo validator e fields

--- api/app/Models/Categoria.php
+++ api/app/Models/Categoria.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class Categoria extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome'];
+
+    private $rules = [
+        'nome' => 'required|max:255'
+    ];
+
+    public function validate($data)
+    {
+        $validator = Validator::make($data, $this->rules);
+        if ($validator->fails()) {
+            return $validator;
+        }
+    }
 
     public function produtos()
     {
--- api/app/Models/Produto.php
+++ api/app/Models/Produto.php
@@ -4,10 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class Produto extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome', 'descricao', 'preco', 'categoria_id'];
+
+    private $rules = [
+        'nome' => 'required',
+        'descricao' => 'required',
+        'preco' => 'required',
+        'categoria_id' => 'required'
+    ];
+
+    public function validate($data)
+    {
+        $validator = Validator::make($data, $this->rules);
+        if ($validator->fails()) {
+            return $validator;
+        }
+    }
 
     public function categoria()
     {
